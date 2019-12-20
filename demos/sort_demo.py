7c866c01985a79ba830aa5f6138ef1541cfad119
Daniel Hopper <daniel.hopper@shopify.com>
2019-12-19 20:18:35 -0500

    restructure modules and add support for 3 more sort keys

--- demos/sort_demo.py
+++ demos/sort_demo.py
@@ -1,14 +1,26 @@
-from user import SpotifyUser
-from utils import features_to_track_ids, playlist_to_track_ids
+import sys
+
+from setlist.user import SpotifyUser
+from setlist.utils import features_to_track_ids, playlist_to_track_ids
 
 TEST_USER = "djhopper"
 TEST_PLAYLIST = "spotify:playlist:5W4m55XzK6XeayO4jBWUU4"
-TEST_SORT_KEY = "tempo"
+SORT_KEYS = ["tempo", "danceability", "energy", "key"]
 
 if __name__ == "__main__":
 
+    if len(sys.argv) > 1:
+        if sys.argv[1] not in SORT_KEYS:
+            print ("Error: sort key must be one of ", SORT_KEYS)
+            sys.exit()
+        else:
+            sort_key = sys.argv[1]
+    else:
+        print ("Usage: %s sort_key" % (sys.argv[0],))
+        sys.exit()
+
     # Authenticate and get the SpotifyUser object
-    user = SpotifyUser("djhopper")
+    user = SpotifyUser(TEST_USER)
 
     # Query for playlist and split into just the track IDs
     original_playlist = user.get_playlist(TEST_PLAYLIST)
@@ -18,7 +30,7 @@ if __name__ == "__main__":
     original_features = user.get_audio_features(original_tracks)
 
     # Sort tracks by desired feature
-    new_features = sorted(original_features, key=lambda x: x[TEST_SORT_KEY])
+    new_features = sorted(original_features, key=lambda x: x[sort_key])
 
     # Extract track IDs and modify exisitng playlist
     new_tracks = features_to_track_ids(new_features)
